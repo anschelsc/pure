f80ed29b424f4a079eb0612a9d3bab19812ac4b7
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2010-12-23 21:11:33 -0500

    Finished with i/o.

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"io/ioutil"
+	"flag"
+	"log"
 )
 
-var test = []string{"a", "`ab", "`ka", "``kab", "`ix", "```sxyz"}
-
 func main() {
-	for _, s := range test {
-		fmt.Printf("%s\t%s\n", s, parse([]byte(s)))
+	flag.Parse()
+	var input []byte
+	if flag.NArg() == 0 {
+		var err os.Error
+		input, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Exitln(err)
+		}
+	} else {
+		file, err := os.Open(flag.Arg(0), os.O_RDONLY, 0)
+		if err != nil {
+			log.Exitln(err)
+		}
+		defer file.Close()
+		input, err = ioutil.ReadAll(file)
+		if err != nil {
+			log.Exitln(err)
+		}
 	}
+	parsed := parse(input)
+	fmt.Println(parsed)
 }
