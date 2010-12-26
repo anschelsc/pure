aca676538d7a0c48b7369743afdf82ee8f7c5059
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2010-12-25 20:50:51 -0500

    Working(?) eliminate().

--- eliminate.go
+++ eliminate.go
@@ -19,3 +19,17 @@ func dumbParse(raw []byte) simple {
 	}
 	return char(raw[0])
 }
+
+func eliminate(f simple, c char) Func {
+	if fCh, ok := f.(char); ok && fCh == c {
+		return I
+	}
+	if !f.contains(c) {
+		return K.apply(f)
+	}
+	p := f.(pair) //safe since only pair and char implement simple, and char is always caught above
+	if second, ok := p[1].(char); ok && second == c && !p[0].(simple).contains(c) {
+		return p[0]
+	}
+	return S.apply(eliminate(p[0].(simple), c)).apply(eliminate(p[1].(simple), c))
+}
